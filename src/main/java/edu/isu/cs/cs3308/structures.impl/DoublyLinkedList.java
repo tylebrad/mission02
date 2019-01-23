40c66d9431139ed79826ed942b597a81a9b19844
tylebrad <tylebrad@Isu.edu>
2019-01-23 10:43:20 -0700

    Planning Stage 1/23 10:45
    Simply beggining to make classes and managing them all correctly

---
+++ src/main/java/edu/isu/cs/cs3308/structures.impl/DoublyLinkedList.java
@@ -0,0 +1,70 @@
+/**
+ * Author: Bradley Tyler
+ * Date: 1/23/2019
+ * Project: Homework01
+ * Class: CS 3308
+ * Professor: Isaac Griffith
+ */
+package edu.isu.cs.cs3308.structures.impl;
+
+public class DoublyLinkedList<E> implements List<E> {
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+
+    @Override
+    public void addLast(E element) {
+
+    }
+
+    @Override
+    public void addFirst(E element) {
+
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public void insert(E element, int index) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void printList() {
+
+    }
+}
